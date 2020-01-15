23476063bbfe2051a5cec1ce8339003932b28cc9
duracell27 <volodymyrshmidt@gmail.com>
2020-01-15 16:59:30 +0200

    14 lesson

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
+import cls from './Navbar.module.css';
 
 const Navbar = () => {
+  console.log(cls);
     return(
-        <nav className='nav'>
-        <div>
+        <nav className={cls.nav}>
+        <div className={`${cls.item} ${cls.active}`}>
           <a href='#'>Profile</a>
         </div>
-        <div>
+        <div className={cls.item}>
           <a href='#'>Messages</a>
         </div>
-        <div>
+        <div className={cls.item}>
           <a href='#'>News</a>
         </div>
-        <div>
+        <div className={cls.item}>
           <a href='#'>Music</a>
         </div>
-        <div>
+        <div className={cls.item}>
           <a href='#'>Settings</a>
         </div>
       </nav>
--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import cls from './Profile.module.css';
 
 const Profile = () => {
     return(
-        <div className='content'>
+        <div className={cls.content}>
         <div>
         <img src='https://panovatravel.com/wp-content/uploads/2017/12/original-1800955822-1200x300.jpg'></img>
         </div>
