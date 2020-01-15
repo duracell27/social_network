b2fd4c62254279850456d0e52b55ba4349439f18
duracell27 <volodymyrshmidt@gmail.com>
2020-01-15 23:46:43 +0200

    18 lesson

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './Components/Header/Header';
 import Navbar from './Components/Navbar/Navbar';
 import Profile from './Components/Profile/Profile';
+import Dialogs from './Components/Dialogs/Dialogs';
 
 const App = () => {
   return (
@@ -10,7 +11,12 @@ const App = () => {
     <div className="app-wrapper">
       <Header />
       <Navbar />
-      <Profile />
+      
+      <div className='app-wrapper-content'>
+        <Dialogs />
+        {/* <Profile /> */}
+      </div>
+
     </div>
   );
 }
---
+++ src/Components/Dialogs/Dialogs.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import cls from './Dialogs.module.css';
+
+const Dialogs = () => {
+    return(
+        <div>dialogs</div>
+        
+    );
+}
+
+export default Dialogs;
