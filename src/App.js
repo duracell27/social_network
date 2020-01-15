320dae8b19e17974710994d9097b54501b032029
duracell27 <volodymyrshmidt@gmail.com>
2020-01-16 00:20:42 +0200

    19 lesson

--- src/App.js
+++ src/App.js
@@ -4,20 +4,23 @@ import Header from './Components/Header/Header';
 import Navbar from './Components/Navbar/Navbar';
 import Profile from './Components/Profile/Profile';
 import Dialogs from './Components/Dialogs/Dialogs';
+import { Route, BrowserRouter } from 'react-router-dom';
 
 const App = () => {
   return (
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header />
+        <Navbar />
 
-    <div className="app-wrapper">
-      <Header />
-      <Navbar />
-      
-      <div className='app-wrapper-content'>
-        <Dialogs />
-        {/* <Profile /> */}
+        <div className='app-wrapper-content'>
+          <Route path='/dialogs' component={Dialogs} />
+          <Route path='/profile' component={Profile} />
+
+        </div>
+
       </div>
-
-    </div>
+    </BrowserRouter>
   );
 }
 
