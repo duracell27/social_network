e807df4073502427530d0b27d4ea3ca74d241bfc
duracell27 <volodymyrshmidt@gmail.com>
2020-01-16 00:03:07 +0200

    nav router error

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,10 +6,10 @@ const Navbar = () => {
     return(
         <nav className={cls.nav}>
         <div className={`${cls.item} ${cls.active}`}>
-          <a href='#'>Profile</a>
+          <a href='/profile'>Profile</a>
         </div>
         <div className={cls.item}>
-          <a href='#'>Messages</a>
+          <a href='/dialogs'>Messages</a>
         </div>
         <div className={cls.item}>
           <a href='#'>News</a>
