989a4e6f0a7b12645e8beb35b2a7cd3779e9c685
duracell27 <volodymyrshmidt@gmail.com>
2020-01-16 17:50:13 +0200

    22 lesson

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
 import cls from './Dialogs.module.css';
+import { NavLink } from 'react-router-dom';
 
 const Dialogs = () => {
     return(
-        <div>dialogs</div>
+        <div className={cls.dialogs}>
+            <div className={cls.dialogsItems}>
+                <div className={`${cls.dialog} ${cls.active}`}>
+                    <NavLink to="/dialogs/1">dima </NavLink>
+                </div>
+                <div className={cls.dialog}>
+                    <NavLink to="/dialogs/2">rostik </NavLink>
+                </div>
+                <div className={cls.dialog}>
+                    <NavLink to="/dialogs/3">anton </NavLink>
+                </div>
+                <div className={cls.dialog}>
+                    <NavLink to="/dialogs/4">sasha </NavLink>
+                </div>
+                <div className={cls.dialog}>
+                    <NavLink to="/dialogs/5">roman </NavLink>
+                </div>
+            </div>
+            <div className={cls.messges}>
+                <div className={cls.message}>Hi</div>
+                <div className={cls.message}>Hello</div>
+                <div className={cls.message}>how are you</div>
+                <div className={cls.message}>fine</div>
+            </div>
+        </div>
         
     );
 }
