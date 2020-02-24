bbce2a7a566310c259afdb497d0e02663d7c4d0d
duracell27 <volodymyrshmidt@gmail.com>
2020-02-24 16:59:56 +0200

    55 lesson

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -9,12 +9,35 @@ class Users extends React.Component {
         super(props);
         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
             this.props.setUsers(response.data.items);
+            debugger;
+            this.props.setTotalUsersCount(response.data.totalCount);
+        });
+    }
+
+    onPageChanged = (current) => {
+        this.props.setCurrentPage(current);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${current}&count=${this.props.pageSize}`).then(response => {
+            this.props.setUsers(response.data.items);
         });
     }
 
     render() {
+
+        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
+
+        let pages = [];
+
+        for(let i=1; i<=pagesCount; i++ ){
+            pages.push(i);
+        }
+
         return <div className={cls.usersWrap} >
-            <button onClick={this.showusers}> Show People </button>
+            <div className={cls.pagination}>
+                {pages.map(p => {
+                    return <span className={this.props.currentPage === p && cls.selectedPage} onClick={()=>{this.onPageChanged(p)}}>{p}</span>    
+                })}
+            </div>
+    
             {
                 this.props.users.map((user) => <div className={cls.userItem} key={user.id}>
                     <div className={cls.ava}>
--- src/Components/Users/UsersContainer.jsx
+++ src/Components/Users/UsersContainer.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Users from './Users';
-import { followAC, unfollowAC, setUsercAC } from '../../redux/usersReducer';
+import { followAC, unfollowAC, setUsercAC, setCurrentPageAC, setTotalUsersCountAC } from '../../redux/usersReducer';
 
 let mapStateToProps = (state) => {
     return {
-        users: state.users.users
+        users: state.users.users,
+        pageSize: state.users.pageSize,
+        totalUsersCount: state.users.totalUsersCount,
+        currentPage: state.users.currentPage
     }
 }
 
 let mapDispatchToProps = (dispatch) => {
     return {
         follow: (userId) =>{
-            debugger;
             dispatch(followAC(userId));
         },
         unfollow: (userId) => {
@@ -20,6 +22,12 @@ let mapDispatchToProps = (dispatch) => {
         },
         setUsers: (users) => {
             dispatch(setUsercAC(users));
+        },
+        setCurrentPage: (current) => {
+            dispatch(setCurrentPageAC(current));
+        },
+        setTotalUsersCount: (totalUsers) => {
+            dispatch(setTotalUsersCountAC(totalUsers));
         }
     }
 }
