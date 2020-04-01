f70a6c8ae8ba379013941b6cd9265b2931722f17
duracell27 <volodymyrshmidt@gmail.com>
2020-04-01 20:54:05 +0300

    Updates

---
+++ precache-manifest.f206e74b4c4c58fb0195fcb5460bf904.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af57a8dc0ea6cdbc95d7eaa3a3d61d00",
+    "url": "/duracell27/social_network/index.html"
+  },
+  {
+    "revision": "b69ca6bd41606a109268",
+    "url": "/duracell27/social_network/static/css/3.9d78c2d9.chunk.css"
+  },
+  {
+    "revision": "722015f72f140205f11d",
+    "url": "/duracell27/social_network/static/css/4.c8a0d24e.chunk.css"
+  },
+  {
+    "revision": "67eb1f4a3a4bc40f505d",
+    "url": "/duracell27/social_network/static/css/5.47b57205.chunk.css"
+  },
+  {
+    "revision": "33190eafe9e9387e7a9c",
+    "url": "/duracell27/social_network/static/css/main.121bdcf2.chunk.css"
+  },
+  {
+    "revision": "743ce2e061ca852a8f79",
+    "url": "/duracell27/social_network/static/js/2.00e62797.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/duracell27/social_network/static/js/2.00e62797.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b69ca6bd41606a109268",
+    "url": "/duracell27/social_network/static/js/3.7289e063.chunk.js"
+  },
+  {
+    "revision": "722015f72f140205f11d",
+    "url": "/duracell27/social_network/static/js/4.c1f99600.chunk.js"
+  },
+  {
+    "revision": "67eb1f4a3a4bc40f505d",
+    "url": "/duracell27/social_network/static/js/5.864addff.chunk.js"
+  },
+  {
+    "revision": "33190eafe9e9387e7a9c",
+    "url": "/duracell27/social_network/static/js/main.d7ef91be.chunk.js"
+  },
+  {
+    "revision": "1d6ecc2ef86a13a1ad3f",
+    "url": "/duracell27/social_network/static/js/runtime-main.9f3b4873.js"
+  },
+  {
+    "revision": "e88bb40ecefdbd2d2f9a02944e23e687",
+    "url": "/duracell27/social_network/static/media/jobfalse.e88bb40e.png"
+  },
+  {
+    "revision": "f59f953fc23383734f4472134ef124e0",
+    "url": "/duracell27/social_network/static/media/jobtrue.f59f953f.png"
+  },
+  {
+    "revision": "ce8e7ab80c30d435d1557087b2a130dc",
+    "url": "/duracell27/social_network/static/media/people.ce8e7ab8.png"
+  },
+  {
+    "revision": "784daf403c0459ff713d2fee5427dcfe",
+    "url": "/duracell27/social_network/static/media/spinner.784daf40.svg"
+  }
+]);
